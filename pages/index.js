31ca208c5a50bb1c69a462dd76aa4d8c97b85628
Jerry Mesinas <jerrymesinas@Rustys-MacBook-Pro.local>
2022-07-30 23:16:17 +0800

    portfolio update

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,44 @@
 import Head from 'next/head' 
 import Image from 'next/image' 
 import Link from 'next/link'
-import styles from '../styles/Home.module.css' 
+import bg from '../public/bg.png'
 
 export default function Home() {
   return (
-    <main>
+    <main style={{backgroundImage: `url(${bg})`}}>
       <header>
           <nav>
-              <ul>
-                <Link href={"/"}>Home</Link>
-                <Link href={"/about"}>About</Link> 
+              <Link href={"/"}>
+                  <a ><img src="jerryUI.svg"></img></a> 
+              </Link>
+              <ul className="" > 
+                <li>
+                  <Link href={"/"}>
+                    <a >About me</a> 
+                  </Link> 
+                </li>
+                <li>
+                  <Link href={"/"}>
+                    <a >Skills</a> 
+                  </Link> 
+                </li>
+                <li>
+                  <Link href={"/"}>
+                    <a >Projects</a> 
+                  </Link> 
+                </li>
+                <li>
+                  <Link href={"/"}>
+                    <a >Contacts</a> 
+                  </Link> 
+                </li>
               </ul>
           </nav>
       </header>
-      <p>Hello this home page</p>
+      <section className="container">
+          <h2>Hello! My names is<br></br><span>Jerry Mesinas Jr.</span></h2>
+          <img></img>
+      </section>
     </main>
   )
 }
